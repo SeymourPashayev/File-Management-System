29ed6005bc76f2ff98bff3e198a69801c6fa815c
Raul Abdullayev <raul.abd2002@gmail.com>
2020-08-27 15:19:29 +0400

    Checkout

--- testForGit/Rename.cpp
+++ testForGit/Rename.cpp
@@ -14,7 +14,7 @@ Rename::Rename(std::filesystem::path& pathTo) {
 // Driver function.
 void Rename::rename()
 {
-	int fileNum = 1;
+	int fileNum{ 1 };
 
 	for (auto& dirEntry : std::filesystem::directory_iterator{ path }) {
 		if (dirEntry.is_regular_file()) {
@@ -31,21 +31,21 @@ void Rename::rename()
 				numOfZeroesToRemove--;
 			}*/
 
-			std::string renameTo = leadingZeroes + std::to_string(fileNum) + "" + newName;
+			std::string renameTo{ leadingZeroes + std::to_string(fileNum) + newName };
 			std::cout << renameTo << "    ";
 			// Add the file extensions back to the newName.
 			renameTo += dirEntryPath.extension().string();
 			std::cout << renameTo << "      ";
 
 			// Create the final path with the new name
-			std::filesystem::path newPath{ dirEntryPath.replace_filename(renameTo) };
-
-			std::cout << newPath << std::endl << std::endl;
+			std::filesystem::path newPath{ dirEntryPath.replace_filename(renameTo) }; //RAUL: replace_filename works on the caller object
+			                                                                          //so dirEntryPath and newPath are the same.
+			std::cout << newPath << std::endl << std::endl;                           //how about using dirEntryPath directly?
 
 			// Rename the file
 			std::filesystem::rename(dirEntry, newPath);
 
-			fileNum++;
+			++fileNum;
 
 			
 		}
@@ -64,7 +64,7 @@ std::string Rename::setNumLeadingZeroes(std::size_t numFilesInDir)
 	std::string zeroes;
 	while (numFilesInDir >= 10) {
 		zeroes += "0";
-		numFilesInDir %= 10;
+		numFilesInDir /= 10; //RAUL: changed %= to /=
 	}
 
 	return zeroes;
@@ -75,8 +75,8 @@ int Rename::zeroesToRemove(int fileNum) {
 	int numToRemove = 0;
 
 	while (fileNum >= 10) {
-		numToRemove++;
-		fileNum %= 10;
+		++numToRemove;
+		fileNum /= 10; //RAUL: changed %= to /=
 	}
 
 	return numToRemove;
@@ -85,6 +85,10 @@ int Rename::zeroesToRemove(int fileNum) {
 // Prompts user for a new name
 std::string Rename::setNewName() {
 	std::cout << std::endl << std::setw(50) << "Input name:" << std::endl;
+
+	if (std::cin.peek() == '\n') {
+		std::cin.ignore();
+	}
 
 	std::string inNewName;
 	std::getline(std::cin, inNewName);
@@ -128,9 +132,10 @@ Rename::RENAME_NEW_NAME Rename::setNewNameSelectivity() {
 // Needed to properly use leading zeroes indentation in one of the rename styles.
 // @Input: std::filesystem::path type path to check.
 // @Output: std::size_t number of files in a directory.
-std::size_t Rename::getNumFilesInDir(std::filesystem::path pathToCheck)
+
+//RAUL: wrapped is_regular_file() inside a lambda, then fed lambda to the count_if(). No problem with overload resolution this way.
+std::size_t Rename::getNumFilesInDir(std::filesystem::path pathToCheck) 
 {
-	using std::filesystem::directory_iterator;
-	using fp = bool (*)(const std::filesystem::path&);
-	return std::count_if(directory_iterator(pathToCheck), directory_iterator{}, (fp)std::filesystem::is_regular_file);
+	auto isRegularFile{ [](auto pathToCheck) {return std::filesystem::is_regular_file(pathToCheck); } };
+	return std::count_if(std::filesystem::directory_iterator(pathToCheck), std::filesystem::directory_iterator{}, isRegularFile);
 }
